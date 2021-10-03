6b19e40c6052ccf801be4d56e2ef9f491b89377a
Noah Mutersbaugh <nmutersb@gmail.com>
2021-10-02 20:03:11 -0400

    Adding folder structure for the application files

---
+++ Application/src/components/NavBar.vue
@@ -0,0 +1,47 @@
+<template>
+  <div id="nav">
+    <img src="@/assets/gvsu_oneline_RGB_white_1024.png" />
+    <div id="nav-wrapper">
+      <div>
+        <router-link to="/">Home</router-link> |
+        <router-link to="/inventory">Inventory</router-link>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "NavBar",
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+#nav {
+  /* height: 2rem; */
+  display: flex;
+  flex-flow: row nowrap;
+  justify-content: space-between;
+  align-items: center;
+  background-color: #0065a4;
+  padding: 30px;
+}
+
+img {
+  height: 2.5rem;
+}
+
+#nav-wrapper {
+    width: 50%;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #000000;
+}
+
+#nav a.router-link-exact-active {
+  color: #ffffff;
+}
+</style>
---
+++ Application/src/views/Inventory.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="Inventory">
+    <NavBar/>
+    <h1>This is an Inventory page</h1>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import NavBar from '@/components/NavBar.vue'
+
+export default {
+  name: 'Inventory',
+  components: {
+    NavBar
+  }
+}
+</script>
