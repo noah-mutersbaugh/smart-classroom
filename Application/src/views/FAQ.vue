642e957f2f11257c025a5999639e8d14a34f04f5
Noah Mutersbaugh <nmutersb@gmail.com>
2021-10-18 17:50:35 -0400

    added background to FAQ

--- Application/src/views/FAQ.vue
+++ Application/src/views/FAQ.vue
@@ -2,7 +2,7 @@
   <div class="d-flex flex-column">
     <NavBar msg="FAQ" />
 
-    <div class="container-md mt-3">
+    <div class="container-md mt-3 p-5 bg-light bg-gradient rounded">
       <h1>Students and Non-Faculty</h1>
       <div class="row">
         <div class="card">
@@ -34,9 +34,13 @@
 
         <div class="card">
           <div class="card-body">
-            <h5 class="card-title">Do Non-Faculty have access to control technology in the room?</h5>
+            <h5 class="card-title">
+              Do Non-Faculty have access to control technology in the room?
+            </h5>
             <p class="card-text">
-              Non-Facult don't have access to pre-set preferences, but they do have access to the technology for presentations and recorded lectures.
+              Non-Facult don't have access to pre-set preferences, but they do
+              have access to the technology for presentations and recorded
+              lectures.
             </p>
           </div>
         </div>
@@ -45,7 +49,10 @@
           <div class="card-header">Quote</div>
           <div class="card-body">
             <blockquote class="blockquote mb-0">
-              <p>I'm so glad to have these classrooms on campus. They make me feel like GVSU is investing in their students.</p>
+              <p>
+                I'm so glad to have these classrooms on campus. They make me
+                feel like GVSU is investing in their students.
+              </p>
               <footer class="blockquote-footer">
                 David Powers in <cite title="Source Title">The Lanthorn</cite>
               </footer>
@@ -65,18 +72,26 @@
 
         <div class="card">
           <div class="card-body">
-            <h5 class="card-title">My settings aren't taking effect. What can I do?</h5>
+            <h5 class="card-title">
+              My settings aren't taking effect. What can I do?
+            </h5>
             <p class="card-text">
-              Go ahead and fill out the Feedback form and we'll be sure to respond as soon as possible with a solution.
+              Go ahead and fill out the Feedback form and we'll be sure to
+              respond as soon as possible with a solution.
             </p>
           </div>
         </div>
 
         <div class="card">
           <div class="card-body">
-            <h5 class="card-title">I'd like to offer a suggestion! Who can I get in contact with?</h5>
+            <h5 class="card-title">
+              I'd like to offer a suggestion! Who can I get in contact with?
+            </h5>
             <p class="card-text">
-              Our Feedback form is the perfect place to offer suggestions! Your feedback will be sent directly to an active email address of the faculty responsible, and you'll receive a confirmation email soon after.
+              Our Feedback form is the perfect place to offer suggestions! Your
+              feedback will be sent directly to an active email address of the
+              faculty responsible, and you'll receive a confirmation email soon
+              after.
             </p>
           </div>
         </div>
@@ -85,10 +100,13 @@
           <div class="card-header">Quote</div>
           <div class="card-body">
             <blockquote class="blockquote mb-0">
-              <p>These smart classrooms are intended to reduce cognitive load on our professors so they can spend more time on their lesson plan, and less time on fiddling with technology. I think we've managed to achieve this goal through the Smart Classroom Initiative.</p>
-              <footer class="blockquote-footer">
-                Noah Mutersbaugh
-              </footer>
+              <p>
+                These smart classrooms are intended to reduce cognitive load on
+                our professors so they can spend more time on their lesson plan,
+                and less time on fiddling with technology. I think we've managed
+                to achieve this goal through the Smart Classroom Initiative.
+              </p>
+              <footer class="blockquote-footer">Noah Mutersbaugh</footer>
             </blockquote>
           </div>
         </div>
