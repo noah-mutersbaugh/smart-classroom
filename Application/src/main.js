61cbafbe230aaa63b84cddc225e50aedda5b9411
Noah Mutersbaugh <nmutersb@gmail.com>
2021-11-02 19:01:06 -0400

    start of login

--- Application/src/main.js
+++ Application/src/main.js
@@ -2,10 +2,18 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+import VueGoogleApi from 'vue-google-api'
 
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 import './assets/main.css'
+
+const config = {
+    apiKey: 'AIzaSyDzXEQHb8Cb0prmQGCsOtsR6bNn8x8Zl5I',
+    clientId: '136856074772-vqegrfk0t4gup6r8qgq7rg00reob65sk.apps.googleusercontent.com',
+    scope: 'profile email'
+}
+Vue.use(VueGoogleApi, config);
 
 Vue.use(BootstrapVue);
 Vue.use(IconsPlugin);
