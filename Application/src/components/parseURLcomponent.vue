4bceffa43d8a73930b65b2993cdb69021110f740
Mohammad Saleh <salehm@mail.gvsu.edu>
2021-11-03 19:17:09 -0400

    button initalized and setting up a couple functions, not done. need to link it to tech settings

---
+++ Application/src/components/parseURLcomponent.vue
@@ -0,0 +1,67 @@
+<script src="https://gvsu.edu/smart-classroom?room=MK-1-111"></script>
+ <template>
+   <div class = "container">
+       <div class="row">
+           <div class= col-md-10>
+               <button onclick="location.href='https://gvsu.edu/smart-classroom?room=MK-1-111'" type="button">
+                  https://gvsu.edu/smart-classroom?room=MK-1-111</button>
+               <button onclick="location.href='https://gvsu.edu/smart-classroom?room=MK-1-111'" type="button">
+                  https://gvsu.edu/smart-classroom?room=MK-1-111</button>
+               <button onclick="location.href='https://gvsu.edu/smart-classroom?room=MK-1-111'" type="button">
+                  https://gvsu.edu/smart-classroom?room=MK-1-111</button>
+             </div>
+       </div>
+    </div>
+
+</template>
+
+<script>
+import Vue from 'vue';
+import bootstrapVue from 'bootstrap-vue';
+//import Button from '../views/Button.vue';
+
+
+
+Vue.use(bootstrapVue);
+export default {
+  name: "button",
+  props: {
+    title: String,
+    body: String,
+    link: String,
+    link_desc: String,
+  },
+};
+
+</script>
+function getLocation(href) {
+    var location = document.createElement("a");
+    location.href = href;
+    // IE doesn't populate all link properties when setting .href with a relative URL,
+    // however .href will return an absolute URL which then can be used on itself
+    // to populate these additional fields.
+    if (location.host == "") {
+      location.href = location.href;
+    }
+    return location;
+};
+var room = this.$route.query.room
+function notLocation(href) {    //Hypertext REFerenc
+    var room = this.$route.query.room;
+    location.href = href;
+    // IE doesn't populate all link properties when setting .href with a relative URL,
+    // however .href will return an absolute URL which then can be used on itself
+    // to populate these additional fields.
+    if (location.host == "" && location.database) {
+        if(loggedIn()&&ValidUser()){
+             location.href = location.href
+             //techSettings(room)
+             
+        }
+    
+    }
+    return location;
+};
+//check to see if valid
+function Location(href) { }
+//check database to see if its in the location
