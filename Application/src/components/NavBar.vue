d39c8ff16c4dafa991eff64a2b8b77ceff0b5a49
Noah Mutersbaugh <nmutersb@gmail.com>
2021-11-08 14:09:48 -0500

    misc

--- Application/src/components/NavBar.vue
+++ Application/src/components/NavBar.vue
@@ -1,19 +1,37 @@
 <template>
   <div>
     <b-navbar id="header" toggleable="lg" type="dark">
-      <b-navbar-brand to="/" class="ms-5 me-auto">
+      <b-navbar-brand to="/" class="ms-5">
         <img alt="GVSU Logo" src="@/assets/gvsu_oneline_RGB_white_1024.png" />
       </b-navbar-brand>
-			<div id="google-signin-btn" class="g-signin2 me-5" data-onsuccess="onSignIn" @click="signIn"></div>
-			<button class="me-5" @click="logOut()">Log Out</button>
+
+      <b-navbar-toggle target="nav-collapse1"></b-navbar-toggle>
+
+      <b-collapse id="nav-collapse1" is-nav>
+        <b-navbar-nav class="ms-auto mx-5">
+					<b-nav-item-dropdown right>
+          <!-- Using 'button-content' slot -->
+          <template #button-content>
+            <em>User</em>
+          </template>
+          <div
+              id="google-signin-btn"
+              class="g-signin2 me-5"
+              data-onsuccess="onSignIn"
+              @click="signIn"
+            ></div>
+            <b-nav-item @click="logOut()">Log Out</b-nav-item>
+        </b-nav-item-dropdown>
+        </b-navbar-nav>
+      </b-collapse>
     </b-navbar>
 
     <h1 class="ms-5">{{ msg }}</h1>
 
     <b-navbar id="navigation" toggleable="md">
-      <b-navbar-toggle target="nav-collapse" class="ms-5"></b-navbar-toggle>
+      <b-navbar-toggle target="nav-collapse2" class="ms-5"></b-navbar-toggle>
 
-      <b-collapse id="nav-collapse" class="ms-5" is-nav>
+      <b-collapse id="nav-collapse2" class="ms-5" is-nav>
         <b-navbar-nav id="nav-list" class="mr-auto">
           <b-nav-item to="/inventory">Inventory</b-nav-item>
           <b-nav-item to="/tech-specs">Tech Specs</b-nav-item>
@@ -51,11 +69,12 @@ export default {
     });
   },
   mounted() {
-		// Explicitly render the google sign-in button. Had to use `window.gapi....` to get past the vue-google-api limitations
-		window.gapi.signin2.render('google-signin-btn', { // this is the button "id"
-      onsuccess: this.signIn // note, no "()" here
-    })
-	},
+    // Explicitly render the google sign-in button. Had to use `window.gapi....` to get past the vue-google-api limitations
+    window.gapi.signin2.render("google-signin-btn", {
+      // this is the button "id"
+      onsuccess: this.signIn, // note, no "()" here
+    });
+  },
   watch: {
     // Watch for changes in the store state
     "$store.state.loggedIn": function () {
