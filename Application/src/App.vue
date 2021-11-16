a9d60d84efaa677807c1e1aff1f5551ef65f8fae
Noah Mutersbaugh <nmutersb@gmail.com>
2021-11-16 05:28:43 -0500

    fixed merge issue between development branch and webdev_david branch

--- Application/src/App.vue
+++ Application/src/App.vue
@@ -5,9 +5,9 @@
 </template>
 
 <script>
-import Vue from 'vue'
-import VueAxios from 'vue-axios';
-import axios from 'axios';
+import Vue from "vue";
+import VueAxios from "vue-axios";
+import axios from "axios";
 
 Vue.use(VueAxios, axios);
 
@@ -17,28 +17,43 @@ export default {
   data() {
     return {
       email: "",
-      dbemail: ""
-    }
+      dbemail: "",
+    };
   },
   async created() {
     // On creation, check signed in status and set store state
     var resp = await axios.get(db_URL_Professors);
+    var room = this.$route.query.room;
 
     this.$gapi.isSignedIn().then((localLoggedInStatus) => {
       this.$store.commit(localLoggedInStatus ? "logIn" : "logOut");
       this.$gapi.currentUser().then((profile) => {
         this.email = profile.email; // This is null if the 'email' scope is not present.));
-      });
-      var professors = [];
-      professors = resp.data;
-      for (let element of professors) {
-        if (element.email === "Jonathan.Engelsma@gvsu.edu"){
-          if (element.permission === 0){
-            alert(JSON.stringify(element));
-            break;
+
+        var professors = [];
+        professors = resp.data;
+        for (let element of professors) {
+          // var userRoom = `${element.buildingabbr}-${element.num}`;
+          var userRoom = element.buildingabbr + element.num;
+          if (userRoom === room) {
+            if (element.email === this.email) {
+              if (element.permission === 0) {
+                //alert(JSON.stringify(element));
+                alert(
+                  "Applied " +
+                    element.profname +
+                    "'s settings to room " +
+                    room +
+                    "."
+                );
+                break;
+              }
+            }
+          } else {
+            alert("User is not at the room, so settings weren't applied.");
           }
         }
-      }
+      });
     });
   },
 };
--- Application/src/views/TechSettings.vue
+++ Application/src/views/TechSettings.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="d-flex flex-column">
     <NavBar msg="Technology Settings"/>
-    <building-classroom-selector></building-classroom-selector>
+    <!-- <building-classroom-selector></building-classroom-selector> -->
     <tech-settings-table></tech-settings-table>
   </div>
 </template>
@@ -10,14 +10,14 @@
 // @ is an alias to /src
 import NavBar from '@/components/NavBar.vue'
 import TechSettingsTable from '../components/TechSettingsTable.vue'
-import BuildingClassroomSelector from '../components/BuildingClassroomSelector.vue'
+// import BuildingClassroomSelector from '../components/BuildingClassroomSelector.vue'
 
 export default {
   name: 'TechSettings',
   components: {
     NavBar,
     TechSettingsTable,
-    BuildingClassroomSelector
+    // BuildingClassroomSelector
   }
 }
 </script>
