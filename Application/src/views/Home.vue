06ad62caf8d0f0c8cab19efbb8855d5b9a987aae
Noah Mutersbaugh <nmutersb@gmail.com>
2021-12-01 16:58:16 -0500

    removed the toast tester from the front page

--- Application/src/views/Home.vue
+++ Application/src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="d-flex flex-column">
     <NavBar msg="GVSU Smart Classrooms" />
-    <ToastTester/>
+    <!-- <ToastTester/> -->
     <div class="container p-4 mt-4 bg-gradient bg-light">
       <div class="row row-cols-1 row-cols-md-2 g-4 justify-content-center">
         <div class="col">
@@ -61,7 +61,7 @@
 // @ is an alias to /src
 import NavBar from "@/components/NavBar.vue";
 import Card from "@/components/Card.vue";
-import ToastTester from "@/components/ToastTester.vue";
+// import ToastTester from "@/components/ToastTester.vue";
 
 
 export default {
@@ -69,7 +69,7 @@ export default {
   components: {
     NavBar,
     Card,
-    ToastTester
+    // ToastTester
   },
 };
 </script>
